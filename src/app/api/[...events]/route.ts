95477c2888fdbb6b5f2c4d6a7a1f60b61c92d6b0
intelagense <18331781+intelagense@users.noreply.github.com>
2023-08-16 17:26:43 -0500

    completes initial api

--- src/app/api/[...events]/route.ts
+++ src/app/api/[...events]/route.ts
@@ -6,8 +6,8 @@ connectDB();
 
 export async function GET(req: NextRequest) {
     try {
-        const eventId = req.nextUrl.pathname.split('/').pop();
-        const event = await Event.findOne({ event_id: eventId }).exec();
+        const eventID = req.nextUrl.pathname.split('/').pop();
+        const event = await Event.findOne({ event_id: eventID }).exec();
 
         if (!event) {
             return NextResponse.json({ message: "Event not found" }, { status: 404 });
@@ -20,54 +20,74 @@ export async function GET(req: NextRequest) {
     }
 }
 
-// Add all todos in mongo instance
 export async function POST(req: NextRequest) {
-    // Placeholder code
-    // const body = await req.json();
-    // console.log("POST REQUEST", body);
+    try {
+        const requestBody = await req.json();
 
-    console.log("POST REQUEST");
-    return new Response('OK BUDDY')
+        if (!requestBody) {
+            return NextResponse.json({ message: "This is empty, YEET!" }, { status: 400 });
+        }
+        try {
+            await Event.create(requestBody);
+            console.log("Event created successfully");
+            return NextResponse.json({ message: "Event created" }, { status: 201 });
+        } catch (error) {
+            console.error("Error creating event:", error);
+            return NextResponse.json({ message: "Error creating event" }, { status: 500 });
+        }
+    } catch (jsonError) {
+        console.error("Error parsing JSON:", jsonError);
+        return NextResponse.json({ message: "Error parsing JSON" }, { status: 400 });
+    }
 }
 
+export async function PUT(req: NextRequest) {
+    const eventID = req.nextUrl.pathname.split('/').pop();
 
-// router.post('/events', (req, res, next) => {
-//   console.log(req.body)
-//   // create event
-//   if (req.body) {
-//     Event.create(req.body)
-//       .then((data) => res.json(data))
-//       .catch(next)
-//   } else {
-//     res.status(400).json({
-//       error: 'This is empty, YEET!'
-//     })
-//   }
-// });
+    if (!eventID) {
+        return { status: 400, text: 'Missing event ID' };
+    }
 
-// router.put('/events/:event_id', (req, res, next) => {
-//   // update event to add volunteers
-//   const event_id = req.params.event_id;
-//   const { volunteer } = req.body;
+    const volunteer = await req.json();
 
-//   Event.findOneAndUpdate(
-//     { event_id },
-//     { $push: { volunteers: volunteer } },
-//     { new: true }
-//   )
-//     .then((updatedEvent) => {
-//       if (!updatedEvent) {
-//         return res.status(404).json({ error: 'Event not found' });
-//       }
-//       res.json(updatedEvent);
-//     })
-//     .catch(next);
-// });
+    if (!volunteer) {
+        return { status: 400, text: 'Missing event ID' };
+    }
 
-// router.delete('/events/:event_id', (req, res, next) => {
-//   Event.findOneAndDelete(req.params.event_id)
-//     .then((data) => res.json(data))
-//     .catch(next);
-// });
+    try {
 
-// module.exports = router;
+        const result = await Event.findOneAndUpdate(
+            { event_id: eventID },
+            { $push: { volunteers: volunteer } },
+            { new: true }
+        );
+        if (!result) {
+            return { status: 404, text: 'Event not found' };
+        }
+
+        return { status: 204 };
+    } catch (error) {
+        console.error("Error deleting event:", error);
+        return { status: 500, text: 'Internal Server Error' };
+    }
+}
+
+export async function DELETE(req: NextRequest) {
+    const eventID = req.nextUrl.pathname.split('/').pop();
+
+    if (!eventID) {
+        return { status: 400, text: 'Missing event ID' };
+    }
+
+    try {
+        const result = await Event.findOneAndDelete({ event_id: eventID });
+        if (!result) {
+            return { status: 404, text: 'Event not found' };
+        }
+
+        return { status: 204 };
+    } catch (error) {
+        console.error("Error deleting event:", error);
+        return { status: 500, text: 'Internal Server Error' };
+    }
+}
