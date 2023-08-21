6c4f0134add49d639572e18af8d0aaa0a7871106
intelagense <intelagense@gmail.com>
2023-08-21 11:07:41 -0500

    fix form error and add DOMAIN env

--- src/app/events/[eventID]/edit/page.tsx
+++ src/app/events/[eventID]/edit/page.tsx
@@ -4,7 +4,7 @@ import { QRCodeSVG } from "qrcode.react"
 export default function edit({ params }: { params: { eventID: string } }) {
   const makeApiCall = async () => {
 
-    await fetch('/api', {
+    await fetch(`/api/events/${params.eventID}`, {
       method: 'GET',
       // body: JSON.stringify({Hellow: 'world'})
     })
@@ -12,9 +12,9 @@ export default function edit({ params }: { params: { eventID: string } }) {
   
   return (
     <>
-    <section className="bg-gray-100">
+    <section className="bg-gray-300">
     <form>
-      <div className="grid gap-6 mx-2.5 mb-6 md:grid-cols-3">
+      <div className="grid gap-6 mx-2.5 pb-6 md:grid-cols-3">
         <div className="col-span-2 md:col-span-2">
           <h2>Create or Edit Event</h2>
           <div>
@@ -40,8 +40,8 @@ export default function edit({ params }: { params: { eventID: string } }) {
           <div className="h-96 bg-white">
             Code and Coffee          
             <div className="flex flex-col items-center justify-center">
-                <QRCodeSVG className='flex content-center' value={`${window.location.origin}/${params.eventID}`} size={64} includeMargin={false}/>
-                <h2>{window.location.origin}/{params.eventID}</h2>
+                  <QRCodeSVG className='flex content-center' value={`${process.env.DOMAIN}/${params.eventID}`} size={64} includeMargin={false}/>
+                  <h2>{process.env.DOMAIN}/{params.eventID}</h2>
             </div>
           </div>
 
@@ -50,18 +50,23 @@ export default function edit({ params }: { params: { eventID: string } }) {
       </div>
     </form>
     </section>
-    <section className="bg-gray-200">
+    <hr className="h-1 w-auto mx-auto bg-slate-700 border-0 dark:bg-slate-700"></hr>
+    <section className="bg-gray-300">
       <h2>Add or remove volunteers</h2>
     <form>
-      <div className="grid gap-6 mx-2.5 mb-6 md:grid-cols-2">
+      <div className="grid gap-6 mx-2.5 pb-6 md:grid-cols-2">
         <div>
-          <div>
+          <div className="mb-2">
             <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
             <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
           </div>
-          <div className="mb-6">
+          <div className="mb-2">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
             <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
+            <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
           </div>
           <div className="mb-6">
             <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to list</button>
@@ -69,9 +74,9 @@ export default function edit({ params }: { params: { eventID: string } }) {
         </div>
         <div>
           <ul>
-            <li>Voluteer1 X</li>
-            <li>Voluteer1 X</li>
-            <li>Voluteer1 X</li>
+            <li>Voluteer1 X (Placeholder)</li>
+            <li>Voluteer2 X (Placeholder)</li>
+            <li>Voluteer3 X (Placeholder)</li>
           </ul>
         </div>
       </div> 
