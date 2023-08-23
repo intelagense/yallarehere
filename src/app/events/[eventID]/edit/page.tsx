eeec5412ddf40657eec43008615ec3fc1ac5ef3b
intelagense <18331781+intelagense@users.noreply.github.com>
2023-08-23 10:53:31 -0500

    adds add volunteer functionality

--- src/app/events/[eventID]/edit/page.tsx
+++ src/app/events/[eventID]/edit/page.tsx
@@ -33,6 +33,33 @@ export default function edit({ params }: { params: { eventID: string } }) {
     }
   };
 
+  const addVolunteers = async () => {
+    const name = (document.querySelector("#name") as HTMLInputElement)?.value;
+    const email = (document.querySelector("#email") as HTMLInputElement)?.value;
+    const phone = (document.querySelector("#phone") as HTMLInputElement)?.value;
+
+    const formData = {
+      name: name,
+      email: email,
+      phone: phone,
+    };
+    
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    };
+    
+    try {
+      const response = await fetch(`/api/events/${params.eventID}`, requestOptions);
+      console.log("it worked... but this was your response", response.status)
+    } catch (error) {
+      console.log("error", error )
+    }
+  }
+
   
   return (
     <>
@@ -93,7 +120,7 @@ export default function edit({ params }: { params: { eventID: string } }) {
             <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
           </div>
           <div className="mb-6">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to list</button>
+            <button onClick={addVolunteers} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to list</button>
             </div>
         </div>
         <div>
