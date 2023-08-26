fe8917b950ecdb6432f63f93ed3264cc7b629b70
intelagense <intelagense@gmail.com>
2023-08-25 21:17:37 -0500

    replace DB_STRING with MONGODB_URI

--- src/app/utils/dbConnect.ts
+++ src/app/utils/dbConnect.ts
@@ -2,10 +2,10 @@ import mongoose from 'mongoose'
 
 export default async function connectDB() {
     try {
-        const DB_STRING = process.env.DB_STRING
-        if (!DB_STRING) throw 'MongoDB connection string: "DB_STRING" not provided'
+        const MONGODB_URI = process.env.MONGODB_URI
+        if (!MONGODB_URI) throw 'MongoDB connection string: "MONGODB_URI" not provided'
 
-        const conn = await mongoose.connect(DB_STRING)
+        const conn = await mongoose.connect(MONGODB_URI)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
 
     } catch (err) {
