d2d50c67dc9d5ea40e990c7bb3e54f678a43869d
Eric Wynn Romere <18331781+intelagense@users.noreply.github.com>
2026-02-22 06:52:48 -0600

    clean up old routes

--- src/app/api/telegram/route.ts
+++ src/app/api/telegram/route.ts
@@ -1,20 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Telegraf } from "telegraf";
 
-const botToken = process.env.TELEGRAM_BOT_TOKEN;
-const chatId = process.env.TELEGRAM_CHAT_ID;
-
-if (!botToken || !chatId) {
-    throw new Error("Telegram bot token or chat ID is missing in environment variables.");
-}
-
-const bot = new Telegraf(botToken);
-
 export async function POST(req: NextRequest) {
+    const botToken = process.env.TELEGRAM_BOT_TOKEN;
+    const chatId = process.env.TELEGRAM_CHAT_ID;
+
+    if (!botToken || !chatId) {
+        return NextResponse.json({ message: "Telegram bot token or chat ID is missing." }, { status: 500 });
+    }
+
     try {
+        const bot = new Telegraf(botToken);
         const message = "🙏 Someone is at the door. Please let them in. 🥺";
-        const chatIdNumber = Number(chatId); // Convert chatId to a number
-        await bot.telegram.sendMessage(chatIdNumber, message);
+        await bot.telegram.sendMessage(Number(chatId), message);
         return NextResponse.json({ message: "Message sent successfully." }, { status: 200 });
     } catch (error) {
         console.error("Error sending Telegram message:", error);
